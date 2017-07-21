588ccf65e25503c72162e4fc994baa6991d2bdea
Marc-Antoine Couture <coumarc9@outlook.com>
2017-07-21 17:39:44 -0400

    Adding HydroObjective class

---
+++ src/proc_mapping/objectives/HydroObjective.cc
@@ -0,0 +1,20 @@
+//
+// Created by coumarc9 on 7/21/17.
+//
+
+#include "HydroObjective.h"
+
+namespace proc_mapping
+{
+    HydroObjective::HydroObjective() {
+
+
+
+    }
+
+    HydroObjective::~HydroObjective() {
+
+    }
+}
+
+
---
+++ src/proc_mapping/objectives/HydroObjective.h
@@ -0,0 +1,22 @@
+//
+// Created by coumarc9 on 7/21/17.
+//
+
+#ifndef PROC_MAPPING_HYDROOBJECTIVE_H
+#define PROC_MAPPING_HYDROOBJECTIVE_H
+
+namespace proc_mapping{
+    class HydroObjective {
+    public:
+        HydroObjective();
+        ~HydroObjective();
+
+
+
+    };
+}
+
+
+
+
+#endif //PROC_MAPPING_HYDROOBJECTIVE_H
