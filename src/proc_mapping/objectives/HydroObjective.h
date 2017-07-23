bcd0b6280798c2a62fcd8d491f767a5381c969c0
Marc-Antoine Couture <coumarc9@outlook.com>
2017-07-23 13:45:28 -0700

    Adding dependency on proc_hydro

--- src/proc_mapping/objectives/HydroObjective.h
+++ src/proc_mapping/objectives/HydroObjective.h
@@ -5,11 +5,17 @@
 #ifndef PROC_MAPPING_HYDROOBJECTIVE_H
 #define PROC_MAPPING_HYDROOBJECTIVE_H
 
+#include <proc_hydrophone/PingPose.h>
+
 namespace proc_mapping{
     class HydroObjective {
     public:
         HydroObjective();
         ~HydroObjective();
+
+        void addPing(proc_hydrophone::PingPoseConstPtr &ping);
+
+    private:
 
 
 
